a09339e011dc555e298195a427edccd4efde6bc0
jholland93 <g00363203@gmit.ie>
2020-10-18 16:32:29 +0100

    Lab 3 upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/header';
+import { Content } from './components/content';
+import { Footer } from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav} from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';//using 'as' simplifies BrowseRouter, meaning when we need to access this we can just type 'Router'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+//In order to become a component, the class must extend Component
+class App extends Component {
+  //render method must be added due to adding a class
+  render(){
+    return (
+      <Router>
+      <div className="App">
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+          </Nav>
+        </Navbar> 
+        <br />
+        <Switch>
+          <Route path = '/' component={Content} exact/> {/* by specifying path, path must be exactly this to dsplay component */}
+          <Route path = '/create' component={Header} exact/>
+          <Route path = '/read' component={Footer} exact/>
+        </Switch>
+        
+      </div>
+      </Router>
+    );
+  }//end render method
+}//end class
 
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Footer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>This is the Footer component.</h1>
+            </div>
+        );
+    }//end render method
+}//end component
