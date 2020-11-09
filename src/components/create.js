8ead37e0c181166d57dd39d02545e9b224ce02c4
jholland93 <g00363203@gmit.ie>
2020-11-09 15:50:32 +0000

    lab complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 export class Create extends React.Component {
   constructor() {
@@ -47,6 +48,21 @@ export class Create extends React.Component {
         " " +
         this.state.Poster
     );
+
+    const newMovie = {
+      title: this.state.Title,
+      year: this.state.Year,
+      poster: this.state.Poster,
+    };
+
+    axios
+      .post("http://localhost:4000/api/movies", newMovie) //returns promise
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
